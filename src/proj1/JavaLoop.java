0563944c4412ae83aa6d9e64834ee7bc9632b14d
maat9090 <maat9090@gmail.com>
2020-03-29 17:05:30 +0200

    Java loop

---
+++ src/proj1/JavaLoop.java
@@ -0,0 +1,15 @@
+package proj1;
+
+public class JavaLoop {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 20; i++) {
+			System.out.println(i+1);
+			
+			
+			
+		}
+	}
+
+}

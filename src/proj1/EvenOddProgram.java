899e406869e867fb937f3ffc726c4b9c55868dda
maat9090 <maat9090@gmail.com>
2020-03-29 22:10:45 +0200

    Even Odd program

---
+++ src/proj1/EvenOddProgram.java
@@ -0,0 +1,21 @@
+package proj1;
+
+public class EvenOddProgram {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 21; i++) {
+			if(i% 2 ==0) {
+				System.out.println("The number"+i+"is even");
+			}
+			else {
+				
+				System.out.println("The number"+i+"is odd");
+		
+				
+			}
+			
+		}
+
+	}
+
+}
